d9cf33df8466654d4f227b38faec13b731eac89e
JoseMLuzu <josemanuelluzuriaga@gmail.com>
2026-02-17 15:02:42 -0500

    feat(pokemonDetail): add icons for evolution requirements based on type

--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -20,6 +20,32 @@ import {
   MapPin,
 } from "lucide-react";
 
+const getRequirementIcon = (type) => {
+  switch (type) {
+    case "level":
+      return <ArrowUp className="w-3 h-3 mr-1" />;
+    case "friendship":
+      return <Heart className="w-3 h-3 mr-1" />;
+    case "affection":
+      return <Sparkles className="w-3 h-3 mr-1" />;
+    case "beauty":
+      return <Gem className="w-3 h-3 mr-1" />;
+    case "trade":
+      return <Repeat className="w-3 h-3 mr-1" />;
+    case "time":
+      return <Clock className="w-3 h-3 mr-1" />;
+    case "gender":
+      return <User className="w-3 h-3 mr-1" />;
+    case "move":
+    case "move-type":
+      return <Sword className="w-3 h-3 mr-1" />;
+    case "location":
+      return <MapPin className="w-3 h-3 mr-1" />;
+    default:
+      return null;
+  }
+};
+
 export default function PokemonDetail() {
   const { id } = useParams();
   const navigate = useNavigate();
